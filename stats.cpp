0b81ddd8231d2deba9dbd13c95cc0c3963575fbd
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-05-16 18:20:00 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,20 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<___>& Data) 
+
+{
+    
+    
     //Implement statistics here
+    float minimum;
+    float maximum;
+    
+    if(Data.size())
+    {
+        for(i=0; i< Data.size(); i++)
+        {
+            sum += Data[i];
+        }
+        average_loc = sum/Data.size();
+    }
 }
