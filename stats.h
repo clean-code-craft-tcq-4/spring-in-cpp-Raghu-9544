5c40b03d0ff3b9cc9c6dcefac054cc2a610f9a4a
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-05-19 17:10:23 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -12,3 +12,45 @@ class Stats
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& Data);
 }
+
+class IAlerter
+{
+    public:
+    virtual void set_alert()
+    {}
+};
+
+class EmailAlert : public IAlerter
+{
+    public:
+        bool emailSent;
+        void set_alert()
+        {
+            emailSent = true;
+        }
+};
+
+class LEDAlert : public IAlerter
+{
+public:
+    bool ledGlows;
+    void set_alert()
+    {
+        ledGlows = true;
+    }
+};
+
+
+class StatsAlerter : public EmailAlert, public LEDAlert
+{
+    public:
+    float maxThreshold;
+    std::vector<IAlerter*> alerters_input;
+    void checkAndAlert(const std::vector<float>& );
+    
+    StatsAlerter (const float maxThreshold_temp, std::vector<IAlerter*> &alerters)
+    {
+        maxThreshold = maxThreshold_temp;
+        alerters_input = alerters;
+    }
+};
